733103083a86f7268d4f71c10f35d2ad03a359eb
jjlewis-sa <83170385+jjlewis-sa@users.noreply.github.com>
2024-12-26 23:00:33 +0200

    [A random dad joke generator in javascript and html]

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-var countdiv = 0;
+var counter = 0;
 
 $('#getJokeButton').click(function() {
-        if(document.getElementById("joke_id")){
-            document.getElementById("joke_id").remove();
+        if(document.getElementById("joke_box" + '1')) {
+            document.getElementById("joke_box" + "1").remove();
+            document.getElementsByTagName("br")[0].remove();
+        } else if(document.getElementById("joke_box" + '2')) {
+            document.getElementById("joke_box" + "2").remove();
             document.getElementsByTagName("br")[0].remove();
         } else {
-                
+            
         }
         fetch('https://icanhazdadjoke.com/', {
             headers: {
@@ -14,34 +17,33 @@ $('#getJokeButton').click(function() {
         })
         .then(response => response.json())
         .then(data => {
-            countdiv++;
+            counter++;
+            if (counter > 2) {
+                counter = 1;
+            }
             // Create div element
             var div = document.createElement("div");
             div.className = "joke_box";
-            div.id = "joke_box" + countdiv;
+            div.id = "joke_box" + counter;
             // Create span element
             var span = document.createElement("span");
             span.className = "joke";
-            span.id = "joke_id";
+            span.id = "joke_id" + counter;
             span.innerHTML = data.joke;
             var br = document.createElement("br");
             // Append to another element:
             document.getElementById("joke-section").appendChild(div);
-            document.getElementById("joke_box" + countdiv).appendChild(span);
+            document.getElementById("joke_box" + counter).appendChild(span);
             document.getElementById("joke-section").appendChild(br);
         })
         .catch(error => {
             console.error('Error fetching joke:', error);
         });
     });
-    const fs = null;
-    const fileName = './count.json';
 
     function counter_fn() {
         //get json data
-        var content = JSON.parse(fs.readFileSync(fileName, 'utf8'));
-        // edit or add property
-        content.expiry_date = 999999999999;
+
         //get json data
         var counter = document.getElementById("cntr");
         var count = 0;
@@ -49,7 +51,7 @@ $('#getJokeButton').click(function() {
         count = count + 1;
         counter.innerHTML = count;
         //write file
-        fs.writeFileSync('count.json', JSON.stringify(content));
-        console.log(content);
+        
+        console.log(count);
       }
       window.onload = counter_fn;
