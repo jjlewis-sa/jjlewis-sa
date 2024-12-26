df89c92254af96db2c78b6218b7b96d7d9bbeed6
jjlewis-sa <83170385+jjlewis-sa@users.noreply.github.com>
2024-12-26 19:18:32 +0200

    [A random dad joke generator in javascript and html]

---
+++ script.js
@@ -0,0 +1,28 @@
+    $('#getJokeButton').click(function() {
+        if(document.getElementById("joke_id")){
+            document.getElementById("joke_id").remove();
+            document.getElementsByTagName("br")[0].remove();
+        } else {
+                
+        }
+        fetch('https://icanhazdadjoke.com/', {
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Create span element
+            var span = document.createElement("span");
+            span.className = "joke";
+            span.id = "joke_id";
+            span.innerHTML = data.joke;
+            const br = document.createElement("br");
+            // Append to another element:
+            document.getElementById("joke-section").appendChild(span);
+            document.getElementById("joke-section").appendChild(br);
+        })
+        .catch(error => {
+            console.error('Error fetching joke:', error);
+        });
+    });
