17349762dfc7ce4c3e9d2dff7fbd3a6efff74860
jjlewis-sa <83170385+jjlewis-sa@users.noreply.github.com>
2024-12-27 11:21:12 +0200

    [A random dad joke generator in javascript and html]

--- visitors.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $visitors = GET['visitors'];
-    $arr = array('visitors' => $visitors);
-    
-    $visitordata = json_encode($arr);
-    
-    file_put_contents('count.json', $visitordata);
-
-?>
